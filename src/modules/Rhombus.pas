e3d41df87645c0110e52752723db7876e63b46a1
Emilio Devesa <emilio@MacBook-Pro.local>
2023-08-07 17:24:17 +0200

    Rhombus module implementation

--- src/modules/Rhombus.pas
+++ src/modules/Rhombus.pas
@@ -8,4 +8,30 @@ procedure print(size: integer; internalChar, externalChar: char);
 
 end;
 
+procedure print;
+var position, x, y: integer;
+begin
+	for y:=1 to ((size+1) div 2) do begin
+		position:=(size-(2*y)+3) div 2;
+		for x:=1 to (2*y)-1 do
+			if (x=1)
+			then write(externalChar:position)
+			else if (x=(2*y)-1)
+				then write(externalChar)
+				else write(internalChar);
+
+		writeln;
+	end;
+	for y:=(size div 2) downto 1 do begin
+		position:=(size-(2*y)+3) div 2;
+		for x:=1 to (2*y)-1 do
+			if (x=1)
+			then write(externalChar:position)
+			else if (x=(2*y)-1)
+				then write(externalChar)
+				else write(internalChar);
+		writeln;
+	end;
+end;
+
 end.
