5a9aa58bdfc1fe8a43d90e4516731d992a4ea1ab
Emilio Devesa <emilio@MacBook-Pro.local>
2023-08-07 17:15:09 +0200

    Main menu loop

--- src/Shapes.pas
+++ src/Shapes.pas
@@ -8,6 +8,35 @@ program Shapes;{
 
 import StandardInput; StandardOutput;
 
+function mainMenu: integer;
+var opc: integer;
 begin
-	writeln('Hello, World!');
+	repeat
+		writeln('SHAPES');
+		writeln('1. Square');
+		writeln('2. Triangle');
+		writeln('3. Rhombus');
+		writeln('0. Exit');
+		write('Option?: ');
+		readln(opc);
+		if (opc<0) or (3<opc)
+		then writeln('Invalid option.');
+		mainMenu:=opc;
+	until(0<=opc) and (opc<=3);
+end;
+
+function printShape(option: integer): integer;
+begin
+	case (option) of
+		1: writeln('Print a square');
+		2: writeln('Print a triangle');
+		3: writeln('Print a rhombus');
+		0: {exit};
+	end;
+	printShape:=option;
+end;
+
+begin
+	repeat
+	until (printShape(mainMenu)=0);
 end.
