73c71642d03d52ba3f5cf0a7cb2284629a672132
Emilio Devesa <emilio@MacBook-Pro.local>
2023-08-07 17:20:38 +0200

    Square module implementation

--- src/modules/Square.pas
+++ src/modules/Square.pas
@@ -7,3 +7,17 @@ import StandardOutput;
 procedure print(size: integer; internalChar, externalChar: char);
 
 end;
+
+procedure print;
+var x, y: integer;
+begin
+	for y:=1 to size do begin
+		for x:=1 to size do
+			if (x=1) or (x=size) or (y=1) or (y=size)
+			then write(externalChar)
+			else write(internalChar);
+		writeln;
+	end;
+end;
+
+end.
