e6f5efa3bee1afd090d8a2a5fecd9865d0c895a7
Emilio Devesa <emilio@MacBook-Pro.local>
2023-08-07 17:13:05 +0200

    Main program

---
+++ src/Shapes.pas
@@ -0,0 +1,13 @@
+program Shapes;{
+	Shapes
+	Command Line program written in Pascal ISO 10206 (Extended Pascal) that draws geometric shapes
+
+	Emilio Devesa
+	http://emiliodevesa.wordpress.com
+}
+
+import StandardInput; StandardOutput;
+
+begin
+	writeln('Hello, World!');
+end.
