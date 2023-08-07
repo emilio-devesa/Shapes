b9d01175fac677a899167c937236d8f399243851
Emilio Devesa <emilio@MacBook-Pro.local>
2023-08-07 17:22:42 +0200

    Triangle module implementation

--- src/modules/Triangle.pas
+++ src/modules/Triangle.pas
@@ -8,4 +8,19 @@ procedure print(size: integer; internalChar, externalChar: char);
 
 end;
 
+procedure print;
+var position, x, y: integer;
+begin
+	for y:=1 to size do begin
+		position:=size+1-y;
+		for x:=1 to (2*y)-1 do
+			if (x=1)
+			then write(externalChar:position)
+			else if (x=(2*y)-1) or (y=size)
+				then write(externalChar)
+				else write(internalChar);
+		writeln;
+	end;
+end;
+
 end.
