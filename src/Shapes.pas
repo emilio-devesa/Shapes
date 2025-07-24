418024cea9794a0cb12e602f86bcd7e9a3e55ab0
emilio-devesa <92723521+emilio-devesa@users.noreply.github.com>
2025-07-24 17:59:14 +0200

    Removed blog URL in source file header comment

--- src/Shapes.pas
+++ src/Shapes.pas
@@ -1,9 +1,6 @@
 program Shapes;{
 	Shapes
 	Command Line program written in Pascal ISO 10206 (Extended Pascal) that draws geometric shapes
-
-	Emilio Devesa
-	https://emiliodevesa.wordpress.com/2023/08/04/shapes/
 }
 
 import StandardInput; StandardOutput;
